616def412525902e595a73ca962198ed27ba3383
Steven Liu <stevotennis@gmail.com>
2017-08-27 21:26:35 -0700

    Initial push for the exercises

---
+++ src/BodyPartQuiz.java
@@ -0,0 +1,82 @@
+import java.awt.Frame;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class BodyPartQuiz {
+
+	// Get 3 celebrity photos from the internet, save them to your computer, and fill
+	// in their paths here. To get the path, right click on the image, and copy its Location.
+	String firstImage = "/pathtofile/example.jpg";
+	String secondImage = "/pathtofile/example2.jpg";
+	String thirdImage = "/pathtofile/example3.jpg";
+
+	Frame window = new Frame();
+	
+	private void startQuiz() {
+		// Set the size of the window so that you can only see part of the image.
+			
+		// Make an int variable to hold the score.
+
+		// Ask the user who this person is and store their answer
+
+		// If they got the answer right:
+		// -- Tell them they are correct.
+		// -- Increase the score by 1
+
+		// Otherwise:
+		// -- Tell them they are wrong
+
+		// Use the method below to show the next image
+		// Show the score
+		// .... repeat for all your images.....
+	}
+
+	public void showNextImage() {
+		window.removeAll();
+		window.add(getNextImage());
+		window.pack();
+	}
+
+	public static void main(String[] args) {
+		BodyPartQuiz quiz = new BodyPartQuiz();
+		quiz.initializeGui();
+		quiz.startQuiz();
+	}
+
+	private void initializeGui() {
+		initializeImageList();
+		imageIterator = imageList.iterator();
+		window.setVisible(true);
+		showNextImage();
+	}
+
+	private JLabel loadImage(String fileName) {
+		Icon icon = new ImageIcon(fileName);
+		JLabel image = new JLabel(icon);
+		return image;
+	}
+
+	ArrayList<JLabel> imageList = new ArrayList<JLabel>();
+	Iterator<JLabel> imageIterator;
+
+	private void initializeImageList() {
+		JLabel imageLabel = loadImage(firstImage);
+		imageList.add(imageLabel);
+		imageLabel = loadImage(secondImage);
+		imageList.add(imageLabel);
+		imageLabel = loadImage(thirdImage);
+		imageList.add(imageLabel);
+	}
+
+	private JLabel getNextImage() {
+		if (imageIterator.hasNext())
+			return imageIterator.next();
+		window.dispose();
+		return new JLabel();
+	}
+}
---
+++ src/Jeopardy.java
@@ -0,0 +1,69 @@
+// Copyright Wintriss Technical Schools 2013
+package graphical;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Jeopardy implements ActionListener {
+
+	int score = 0;
+	JLabel scoreBox = new JLabel("0");
+
+	public static void main(String[] args) {
+		new Jeopardy().start();
+	}
+
+	private void start() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.setLayout(new BorderLayout());
+		panel.setLayout(new GridLayout(2, 2));
+		JButton q1 = new JButton();
+		q1.addActionListener(this);
+		panel.add(q1);
+		JButton q2 = new JButton("200");
+		panel.add(q2);
+		q2.addActionListener(this);
+		JButton q3 = new JButton("300");
+		panel.add(q3);
+		q3.addActionListener(this);
+		frame.add(panel);
+		frame.add(makeScorePanel(), BorderLayout.NORTH);
+		frame.setVisible(true);
+		frame.pack();
+		frame.setSize(200, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+	private Component makeScorePanel() {
+		JPanel panel = new JPanel();
+		panel.add(new JLabel("score:"));
+		panel.add(scoreBox);
+		return panel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String answer = JOptionPane.showInputDialog("What is ?");
+		if (answer.equals("yes"))
+			score++;
+		((JButton)arg0.getSource()).setText("");
+		update();
+		
+	}
+
+	private void update() {
+		scoreBox.setText(""+score);
+		
+	}
+
+}
