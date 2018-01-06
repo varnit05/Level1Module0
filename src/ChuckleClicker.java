ebf45eeb093636718604fab757bc9cd9d5fa25d7
varnit05 <league@WorthyMini-5.attlocal.net>
2018-01-06 11:29:00 -0800

    ChuckleClicker, SimonSays(ip)

--- src/ChuckleClicker.java
+++ src/ChuckleClicker.java
@@ -1,14 +1,35 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ChuckleClicker {
+public class ChuckleClicker implements ActionListener {
 public static void main(String[] args) {
-	
+	ChuckleClicker chuck = new ChuckleClicker();
+	chuck.makeButtons();
 }
 
 public void makeButtons() {
+	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	JButton joke = new JButton ();
+	JButton punchline = new JButton();
+	panel.add(punchline);
+	panel.add(joke);
+	frame.add(panel);
+	joke.setText("joke");
+	punchline.setText("punchline");
+	frame.setVisible(true);
+	frame.pack();
+	joke.addActionListener(this);
+	punchline.addActionListener(this);
+	
+	
+	
 	
 //Make a ChuckleClicker class, a main method and a makeButtons method. Put a pop-up in the makeButtons method and get it working. You will need to instantiate your class and call method an instance of it. 
 //
@@ -25,5 +46,11 @@ public void makeButtons() {
 //You might need to move the declaration of your buttons above the the makeButtons() method.
 //
 //Use JOptionPane to print the joke or the punchline depending on which button was clicked.
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+JOptionPane.showMessageDialog(null, "hi");
 }
 }
--- src/SimonSays.java
+++ src/SimonSays.java
@@ -15,7 +15,7 @@ public class SimonSays extends KeyAdapter {
  
  // Complete steps 1 - 7 before you test
  // 1. Make a JFrame variable 
- 
+ JFrame frame = new JFrame();
  	HashMap<Integer,String> images = new HashMap<Integer, String>();
  	private int imageIndex;
  	private int tries = 0;
@@ -23,41 +23,71 @@ public class SimonSays extends KeyAdapter {
  	Date timeAtStart;
  	private  void makeAlbum() {
  // 2. add 4 images which match keyboard keys like this: images.put(new Integer(KeyEvent.VK_UP), "image.jpg");
-  	
+ 		images.put(new Integer(KeyEvent.VK_UP), "keyboard_key_up.png");
+ 		
+ 		images.put(new Integer(KeyEvent.VK_LEFT), "keyboard_key_left.png");
+ 		
+ 		images.put(new Integer(KeyEvent.VK_RIGHT), "keyboard_arrow_right.jpg");
+ 		
+ 		images.put(new Integer(KeyEvent.VK_DOWN), "keyboard_key_down.png");
+ 		
  // 3. Tell the user to "Press the matching key when 'Simon says' otherwise press a different key"
- 
+ JOptionPane.showMessageDialog(null, "Press the matching key when 'simon says' otherwise press a different key");
  // 4. call the method to show an image
-
+ 	showImage();
  	}
 	public void keyPressed(KeyEvent e) {
     	int keyCode = e.getKeyCode();
     	// 16. make a points variable to track the score. tell the user their score at the end.
+    		 
     	//17. if the keyCode matches the imageIndex and "Simon says..."  increase their score
+    	
     	//18.   if the keyCode doesn't match the imageIndex and "Simon didn't say..."  increase their score	
+    	
     	//19. Use the speak method to tell the user if they were correct or not
+    	
     	//13. increment tries by 1
+    		tries += 1;
   	
     	//14. if tries is greater than 9 (or however many you want)
-    	
+    	if (tries > 10) {
+			
+		}
     	//15.    	exit the program
+    	System.exit(0);
 
     	//11. dispose of the frame
+    		frame.dispose();
    	
     	//12. call the method to show an image
-
+    		showImage();
 	}
 	private void showImage() {
     	//5. initialize your frame to a new JFrame()
-	 
+		frame = new JFrame();
     	//6. set the frame to visible
-	
+	frame.setVisible(true);
   	 //frame.add(getNextRandomImage()); //7. rename to the name of your frame
-    	
+    		frame.add(getNextRandomImage());
     	// 8. set the size of the frame 
-     	
+     	frame.setSize(500, 500);
     	// 9. add a key listener to the frame
-	
+	frame.addKeyListener(this);
    	 //10. Use the speak method to either say "Simon says press this key" or "Press this key"
+	Random gen = new Random();
+	simonSays = gen.nextInt(2);
+	if (gen.equals(1)) {
+		speak("Simon says presss this key");
+		
+	}
+	else if (gen.equals(0)) {
+		speak("press this key");
+	}
+	
+	
+	
+	
+	
     	//Hint: use the simonSays int and a random number
 	}
 	private Component getNextRandomImage() {
