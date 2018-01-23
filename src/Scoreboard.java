659be39f8ce49de7478c562a9c662022a5fd1acf
varnit05 <league@iMac.attlocal.net>
2018-01-22 18:32:30 -0800

    Scoreboard(ip)

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,3 +1,6 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -9,22 +12,25 @@ public class Scoreboard {
 	}
 
 	JFrame frame = new JFrame();
-	JPanel timePanel = new JPanel();
 	JPanel VisitorPanel = new JPanel();
 	JPanel HomePanel = new JPanel();
-	JLabel VisitorLabel = new JLabel();
-	JLabel HomeLabel = new JLabel();
+	JPanel MainPanel = new JPanel();
+	JPanel HomeFoulsPanel = new JPanel();
+	JPanel VisitorFoulsPanel = new JPanel();
+	JButton VisitorButton = new JButton();
+	JButton HomeButton = new JButton();
 	JLabel VisitorTimeoutsLabel = new JLabel();
 	JLabel HomeTimeoutsLabel = new JLabel();
 	JLabel HomeFoulslabel = new JLabel();
 	JLabel HomeFoulsLabel = new JLabel();
 	JLabel VisitorFoulsLabel = new JLabel();
-	JLabel VisitorScoreLabel = new JLabel();
-	JLabel HomeScoreLabel = new JLabel();
-	JLabel TimeLabel = new JLabel();
+	JButton VisitorScoreButton = new JButton();
+	JButton HomeScoreButton = new JButton();
+	JButton HomeName = new JButton("Home");
+	JButton VisitorName = new JButton("Visitor");
+	
 
-	String HomeName;
-	String VisitorName;
+
 	int HomeScore;
 	int VisitorScore;
 	int HomeFouls;
@@ -33,51 +39,62 @@ public class Scoreboard {
 	int VistorTimeouts;
 
 	public Scoreboard() {
-		this.HomeName = "HOME";
-		HomeLabel.setText(this.HomeName);
 
-		this.VisitorName = "Visitor";
-		VisitorLabel.setText(this.VisitorName);
 
 		this.HomeFouls = 0;
-		HomeFoulsLabel.setText("Fouls: " + Integer.toString(this.HomeFouls));
+		HomeFoulsLabel.setText(" Fouls: " + Integer.toString(this.HomeFouls));
 
 		this.Visitorfouls = 0;
-		VisitorFoulsLabel.setText("Fouls: " + Integer.toString(this.Visitorfouls));
+		VisitorFoulsLabel.setText(" Fouls: " + Integer.toString(this.Visitorfouls));
 
 		this.HomeTimouts = 3;
-		HomeTimeoutsLabel.setText("Timeouts: " + Integer.toString(this.HomeTimouts));
+		HomeTimeoutsLabel.setText(" Timeouts: " + Integer.toString(this.HomeTimouts));
 
 		this.VistorTimeouts = 3;
-		VisitorTimeoutsLabel.setText("Timeouts: " + Integer.toString(this.VistorTimeouts));
+		VisitorTimeoutsLabel.setText(" Timeouts: " + Integer.toString(this.VistorTimeouts));
 
 		this.HomeScore = 00;
-		HomeScoreLabel.setText("score: " + Integer.toString(this.HomeScore));
+		HomeScoreButton.setText("score: " + Integer.toString(this.HomeScore));
 
 		this.VisitorScore = 00;
-		VisitorScoreLabel.setText("score: " + Integer.toString(this.VisitorScore));
+		VisitorScoreButton.setText("score: " + Integer.toString(this.VisitorScore));
 
 		run();
 	}
 
 	void run() {
-		timePanel.add(TimeLabel);
-		VisitorPanel.add(VisitorFoulsLabel);
-		VisitorPanel.add(VisitorScoreLabel);
-		VisitorPanel.add(VisitorScoreLabel);
-		VisitorPanel.add(VisitorTimeoutsLabel);
+		frame.setSize(1000, 75);
+		MainPanel.setLayout(new BorderLayout());
+		HomePanel.setLayout(new BorderLayout());
+		VisitorPanel.setLayout(new BorderLayout());
+		HomeFoulsPanel.setLayout(new BorderLayout());
+        VisitorFoulsPanel.setLayout(new BorderLayout());
+		
+		VisitorPanel.add(VisitorName,BorderLayout.NORTH);
+		VisitorPanel.add(VisitorScoreButton,BorderLayout.CENTER);
+		VisitorPanel.add(VisitorFoulsPanel,BorderLayout.SOUTH);
+		VisitorFoulsPanel.add(VisitorTimeoutsLabel,BorderLayout.WEST);
+		VisitorFoulsPanel.add(VisitorFoulsLabel,BorderLayout.EAST);
 
-		HomePanel.add(HomeFoulsLabel);
-		HomePanel.add(HomeScoreLabel);
-		HomePanel.add(HomeScoreLabel);
-		HomePanel.add(HomeTimeoutsLabel);
+		HomePanel.add(HomeName,BorderLayout.NORTH);
+		HomePanel.add(HomeScoreButton,BorderLayout.CENTER);
+		HomePanel.add(HomeFoulsPanel, BorderLayout.SOUTH);
+		HomeFoulsPanel.add(HomeTimeoutsLabel,BorderLayout.WEST);
+		HomeFoulsPanel.add(HomeFoulsLabel,BorderLayout.EAST);
+		
 
-		frame.add(timePanel);
-		frame.add(HomePanel);
-		frame.add(VisitorPanel);
+		
+	
+		MainPanel.add(HomePanel,BorderLayout.WEST);
+		MainPanel.add(VisitorPanel,BorderLayout.EAST);
+		
+		frame.add(MainPanel);
 
 		frame.setVisible(true);
 		frame.pack();
 
+	}
+	public void setHomeScore(int Score) {
+		this.HomeScore = Score;
 	}
 }
